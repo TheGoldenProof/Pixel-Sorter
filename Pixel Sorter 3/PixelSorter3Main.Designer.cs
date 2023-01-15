878853e363e2dad669578571e0d24c3e8aec26db
TheGoldenProof <firebirdspiral@gmail.com>
2023-01-15 08:46:20 +0000

    Initial Commit

---
+++ Pixel Sorter 3/PixelSorter3Main.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Pixel_Sorter_3 {
+    partial class PixelSorter3Main {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reverseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shuffleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.alphaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.hueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saturationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saturationHSVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.luminosityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.valueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.sortToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(800, 38);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(62, 34);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(257, 40);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(254, 6);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Enabled = false;
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(257, 40);
+            this.saveAsToolStripMenuItem.Text = "Save &As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(254, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(254, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(257, 40);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // sortToolStripMenuItem
+            // 
+            this.sortToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reverseToolStripMenuItem,
+            this.shuffleToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.alphaToolStripMenuItem,
+            this.redToolStripMenuItem,
+            this.greenToolStripMenuItem,
+            this.blueToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.hueToolStripMenuItem,
+            this.saturationToolStripMenuItem,
+            this.saturationHSVToolStripMenuItem,
+            this.luminosityToolStripMenuItem,
+            this.valueToolStripMenuItem});
+            this.sortToolStripMenuItem.Enabled = false;
+            this.sortToolStripMenuItem.Name = "sortToolStripMenuItem";
+            this.sortToolStripMenuItem.Size = new System.Drawing.Size(68, 34);
+            this.sortToolStripMenuItem.Text = "Sort";
+            // 
+            // reverseToolStripMenuItem
+            // 
+            this.reverseToolStripMenuItem.Name = "reverseToolStripMenuItem";
+            this.reverseToolStripMenuItem.Size = new System.Drawing.Size(334, 40);
+            this.reverseToolStripMenuItem.Text = "Reverse";
+            this.reverseToolStripMenuItem.Click += new System.EventHandler(this.reverseToolStripMenuItem_Click);
+            // 
+            // shuffleToolStripMenuItem
+            // 
+            this.shuffleToolStripMenuItem.CheckOnClick = true;
+            this.shuffleToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.shuffleToolStripMenuItem.Name = "shuffleToolStripMenuItem";
+            this.shuffleToolStripMenuItem.Size = new System.Drawing.Size(334, 40);
+            this.shuffleToolStripMenuItem.Text = "Shuffle Before Sorting";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(331, 6);
+            // 
+            // alphaToolStripMenuItem
+            // 
+            this.alphaToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.alphaToolStripMenuItem.Name = "alphaToolStripMenuItem";
+            this.alphaToolStripMenuItem.Size = new System.Drawing.Size(334, 40);
+            this.alphaToolStripMenuItem.Text = "Alpha";
+            this.alphaToolStripMenuItem.Click += new System.EventHandler(this.alphaToolStripMenuItem_Click);
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(334, 40);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // greenToolStripMenuItem
+            // 
+            this.greenToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
+            this.greenToolStripMenuItem.Size = new System.Drawing.Size(334, 40);
+            this.greenToolStripMenuItem.Text = "Green";
+            this.greenToolStripMenuItem.Click += new System.EventHandler(this.greenToolStripMenuItem_Click);
+            // 
+            // blueToolStripMenuItem
+            // 
+            this.blueToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+            this.blueToolStripMenuItem.Size = new System.Drawing.Size(334, 40);
+            this.blueToolStripMenuItem.Text = "Blue";
+            this.blueToolStripMenuItem.Click += new System.EventHandler(this.blueToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(331, 6);
+            // 
+            // hueToolStripMenuItem
+            // 
+            this.hueToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.hueToolStripMenuItem.Name = "hueToolStripMenuItem";
+            this.hueToolStripMenuItem.Size = new System.Drawing.Size(334, 40);
+            this.hueToolStripMenuItem.Text = "Hue";
+            this.hueToolStripMenuItem.Click += new System.EventHandler(this.hueToolStripMenuItem_Click);
+            // 
+            // saturationToolStripMenuItem
+            // 
+            this.saturationToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.saturationToolStripMenuItem.Name = "saturationToolStripMenuItem";
+            this.saturationToolStripMenuItem.Size = new System.Drawing.Size(334, 40);
+            this.saturationToolStripMenuItem.Text = "Saturation (HSL)";
+            this.saturationToolStripMenuItem.Click += new System.EventHandler(this.saturationToolStripMenuItem_Click);
+            // 
+            // saturationHSVToolStripMenuItem
+            // 
+            this.saturationHSVToolStripMenuItem.Name = "saturationHSVToolStripMenuItem";
+            this.saturationHSVToolStripMenuItem.Size = new System.Drawing.Size(334, 40);
+            this.saturationHSVToolStripMenuItem.Text = "Saturation (HSV)";
+            this.saturationHSVToolStripMenuItem.Click += new System.EventHandler(this.saturationHSVToolStripMenuItem_Click);
+            // 
+            // luminosityToolStripMenuItem
+            // 
+            this.luminosityToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.luminosityToolStripMenuItem.Name = "luminosityToolStripMenuItem";
+            this.luminosityToolStripMenuItem.Size = new System.Drawing.Size(334, 40);
+            this.luminosityToolStripMenuItem.Text = "Lightness";
+            this.luminosityToolStripMenuItem.Click += new System.EventHandler(this.luminosityToolStripMenuItem_Click);
+            // 
+            // valueToolStripMenuItem
+            // 
+            this.valueToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.valueToolStripMenuItem.Name = "valueToolStripMenuItem";
+            this.valueToolStripMenuItem.Size = new System.Drawing.Size(334, 40);
+            this.valueToolStripMenuItem.Text = "Value";
+            this.valueToolStripMenuItem.Click += new System.EventHandler(this.valueToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 38);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 412);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.Resize += new System.EventHandler(this.pictureBox1_Resize);
+            // 
+            // PixelSorter3Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "PixelSorter3Main";
+            this.Text = "Pixel Sorter v3.0-1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator;
+        private ToolStripMenuItem saveAsToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem sortToolStripMenuItem;
+        private ToolStripMenuItem shuffleToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator4;
+        private ToolStripMenuItem alphaToolStripMenuItem;
+        private ToolStripMenuItem redToolStripMenuItem;
+        private ToolStripMenuItem greenToolStripMenuItem;
+        private ToolStripMenuItem blueToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator3;
+        private ToolStripMenuItem hueToolStripMenuItem;
+        private ToolStripMenuItem saturationToolStripMenuItem;
+        private ToolStripMenuItem luminosityToolStripMenuItem;
+        private ToolStripMenuItem valueToolStripMenuItem;
+        private PictureBox pictureBox1;
+        private ToolStripMenuItem saturationHSVToolStripMenuItem;
+        private ToolStripMenuItem reverseToolStripMenuItem;
+    }
+}
---
+++ Pixel Sorter 3/PixelSorter3Main.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pixel_Sorter_3 {
+    public partial class PixelSorter3Main : Form {
+        public enum SortMode {
+            None,
+            Alpha,
+            Red,
+            Green,
+            Blue,
+            Hue,
+            SaturationHSL,
+            SaturationHSV,
+            Lightness,
+            Value,
+        }
+
+        public static float GetSaturationHSL(Color c) {
+            float Xmax = Math.Max(c.R, Math.Max(c.G, c.B))/255.0f;
+            float Xmin = Math.Min(c.R, Math.Min(c.G, c.B));
+            float C = Xmax - Xmin;
+            float L = (Xmax + Xmin) / 2;
+            return L == 0 || L == 1 ? 0 : C/(1-Math.Abs(2*Xmax-C-1));
+        }
+
+        public static float GetSaturationHSV(Color c) {
+            float Xmax = Math.Max(c.R, Math.Max(c.G, c.B))/255.0f;
+            float Xmin = Math.Min(c.R, Math.Min(c.G, c.B));
+            float C = Xmax - Xmin;
+            return Xmax == 0 ? 0 : C / Xmax;
+        }
+
+        public static float GetLightness(Color c) {
+            float Xmax = Math.Max(c.R, Math.Max(c.G, c.B)) / 255.0f;
+            float Xmin = Math.Min(c.R, Math.Min(c.G, c.B));
+            return (Xmax + Xmin) / 2;
+        }
+
+        public static float GetValue(Color c) {
+            return Math.Max(c.R, Math.Max(c.G, c.B)) / 255.0f;
+        }
+
+        public class PixelComparer : Comparer<Color> {
+            SortMode sortMode;
+            public PixelComparer(SortMode mode) {
+                sortMode = mode;
+            }
+
+            public override int Compare(Color x, Color y) {
+                int ret = 0;
+                switch (sortMode) {
+                case SortMode.Alpha:
+                    ret = x.A.CompareTo(y.A);
+                    break;
+                case SortMode.Red:
+                    ret = x.R.CompareTo(y.R);
+                    break;
+                case SortMode.Green:
+                    ret = x.G.CompareTo(y.G);
+                    break;
+                case SortMode.Blue:
+                    ret = x.B.CompareTo(y.B);
+                    break;
+                case SortMode.Hue:
+                    ret = x.GetHue().CompareTo(y.GetHue());
+                    break;
+                case SortMode.SaturationHSL:
+                    ret = GetSaturationHSL(x).CompareTo(GetSaturationHSL(y));
+                    break;
+                case SortMode.SaturationHSV:
+                    ret = GetSaturationHSV(x).CompareTo(GetSaturationHSV(y));
+                    break;
+                case SortMode.Lightness:
+                    ret = GetLightness(x).CompareTo(GetLightness(y));
+                    break;
+                case SortMode.Value:
+                    ret = GetValue(x).CompareTo(GetValue(y));
+                    break;
+                case SortMode.None:
+                default:
+                    return 0;
+                }
+                return ret;
+            }
+        }
+
+        private static void Shuffle<T>(IList<T> list) {
+            Random rand = new Random();
+            int n = list.Count;
+
+            for (int i = list.Count - 1; i > 1; i--) {
+                int rnd = rand.Next(i + 1);
+
+                T value = list[rnd];
+                list[rnd] = list[i];
+                list[i] = value;
+            }
+        }
+
+        OpenFileDialog ofd;
+        SaveFileDialog sfd;
+        Bitmap img;
+
+        private void sort(SortMode sortMode) {
+            List<Color> pixels = new List<Color>(img.Width * img.Height);
+
+            for(int y = 0; y < img.Height; y++)
+                for(int x = 0; x < img.Width; x++)
+                    pixels.Add(img.GetPixel(x, y));
+
+            if (shuffleToolStripMenuItem.Checked)
+                Shuffle(pixels);
+
+            pixels.Sort(new PixelComparer(sortMode));
+
+            for(int y = 0; y < img.Height; y++)
+                for(int x = 0; x < img.Width; x++)
+                    img.SetPixel(x, y, pixels[y * img.Width + x]);
+
+            pictureBox1.Refresh();
+        }
+
+        private void reverse() {
+            List<Color> pixels = new List<Color>(img.Width * img.Height);
+
+            for(int y = 0; y < img.Height; y++)
+                for(int x = 0; x < img.Width; x++)
+                    pixels.Add(img.GetPixel(x, y));
+
+            pixels.Reverse();
+
+            for(int y = 0; y < img.Height; y++)
+                for(int x = 0; x < img.Width; x++)
+                    img.SetPixel(x, y, pixels[y * img.Width + x]);
+
+            pictureBox1.Refresh();
+        }
+
+        public PixelSorter3Main() {
+            ofd = new OpenFileDialog {
+                AddToRecent = true,
+                CheckFileExists = true,
+                Filter = "Image Files (*.png;*.jpg;*.jpeg;*.gif;*.webp)|*.png;*.jpg;*.jpeg;*.gif;*.webp|All files (*.*)|*.*",
+                FilterIndex = 0,
+                RestoreDirectory = true,
+                Title = "Open Image",
+            };
+
+            sfd = new SaveFileDialog {
+                AddToRecent = true,
+                CheckWriteAccess = true,
+                DereferenceLinks = true,
+                Filter = "All files (*.*)|*.*",
+                OverwritePrompt = true,
+                RestoreDirectory = true,
+                Title = "Save Image",
+            };
+
+            img = new Bitmap(1, 1);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e) {
+            e.Graphics.Clear(TransparencyKey);
+            e.Graphics.DrawImage(img, 0, 0, pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void pictureBox1_Resize(object sender, EventArgs e) {
+            pictureBox1.Refresh();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) {
+            string filePath = string.Empty;
+
+            if (ofd.ShowDialog() == DialogResult.OK) {
+                filePath = ofd.FileName;
+                img = new Bitmap(filePath);
+                //pictureBox1.Image = img;
+                pictureBox1.Refresh();
+                sortToolStripMenuItem.Enabled = true;
+                saveAsToolStripMenuItem.Enabled = true;
+                alphaToolStripMenuItem.Enabled = Image.IsAlphaPixelFormat(img.PixelFormat);
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (sfd.ShowDialog() == DialogResult.OK) {
+                img.Save(sfd.FileName);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void reverseToolStripMenuItem_Click(object sender, EventArgs e) {
+            reverse();
+        }
+
+        private void alphaToolStripMenuItem_Click(object sender, EventArgs e) {
+            sort(SortMode.Alpha);
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e) {
+            sort(SortMode.Red);
+        }
+
+        private void greenToolStripMenuItem_Click(object sender, EventArgs e) {
+            sort(SortMode.Green);
+        }
+
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e) {
+            sort(SortMode.Blue);
+        }
+
+        private void hueToolStripMenuItem_Click(object sender, EventArgs e) {
+            sort(SortMode.Hue);
+        }
+
+        private void saturationToolStripMenuItem_Click(object sender, EventArgs e) {
+            sort(SortMode.SaturationHSL);
+        }
+
+        private void saturationHSVToolStripMenuItem_Click(object sender, EventArgs e) {
+            sort(SortMode.SaturationHSV);
+        }
+
+        private void luminosityToolStripMenuItem_Click(object sender, EventArgs e) {
+            sort(SortMode.Lightness);
+        }
+
+        private void valueToolStripMenuItem_Click(object sender, EventArgs e) {
+            sort(SortMode.Value);
+        }
+    }
+}
